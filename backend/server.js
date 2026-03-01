25a072890a2734ef4258768bd158cd9caeda0928
Firaol getu <fgetu90@gmail.com>
2026-03-01 13:27:41 +0300

    all changes.

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,8 @@
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
